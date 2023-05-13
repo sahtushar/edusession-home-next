ab0f3dff5c62e819cc844e9b52fc827160bb254e
sahtushar <sahtushar@gmail.com>
2023-05-13 21:42:43 +0530

    border in banner"

--- components/sections/Banners.js
+++ components/sections/Banners.js
@@ -49,10 +49,10 @@ const Banners = ({
     <section {...props} className={outerClasses}>
       <div>
         <div className={innerClasses}>
-            <div className="img1">
+            <div className="img1 dashed" >
              <MyImage src={result10} alt="12th results 2023" /> 
             </div>
-            <div className="img1">
+            <div className="img1 dashed">
              <MyImage src={result12} alt="10th results 2023" /> 
             </div>
         </div>
